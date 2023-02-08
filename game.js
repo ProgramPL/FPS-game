295733b425e71dc758d81722fca0abf53462af15
Dylan X <72346428+ProgramPL@users.noreply.github.com>
2023-02-08 17:11:45 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,41 +1,95 @@
-//Define variables
-let score = 0;
-let health = 100;
-let ammo = 0;
-let currentWeapon = 'pistol';
-let weapons = ['pistol', 'shotgun', 'rifle', 'sniper'];
-let enemies = [];
-let level = 1;
-
-//Function to shoot weapon
-function shootWeapon() {
-    if (ammo > 0) {
-        switch (currentWeapon) {
-            case 'pistol':
-                //Code for pistol shooting
-                break;
-            case 'shotgun':
-                //Code for shotgun shooting
-                break;
-            case 'rifle':
-                //Code for rifle shooting
-                break;
-            case 'sniper':
-                //Code for sniper shooting
-                break;
+//Create the game
+let game = {
+    map: {
+        size: 10,
+        walls: [],
+        objectives: []
+    },
+    teams: {
+        team1: {
+            players: [],
+            score: 0,
+            objectives: []
+        },
+        team2: {
+            players: [],
+            score: 0,
+            objectives: []
         }
-        ammo--;
+    },
+    players: [],
+    weapons: [],
+    items: [],
+    effects: [],
+    round: {
+        number: 0,
+        state: 'idle',
+        startTime: 0
+    },
+    gameModes: [
+        {
+            name: 'standard',
+            type: 'team-deathmatch',
+            winCondition: 'first-team-to-x-kills'
+        },
+        {
+            name: 'spike-rush',
+            type: 'objective',
+            winCondition: 'plant-the-spike'
+        },
+        {
+            name: 'horde',
+            type: 'survival',
+            winCondition: 'survive-all-waves'
+        }
+    ],
+    maps: [
+        {
+            name: 'Icebox',
+            type: 'snow',
+            size: 10
+        },
+        {
+            name: 'Bind',
+            type: 'desert',
+            size: 10
+        },
+        {
+            name: 'Split',
+            type: 'urban',
+            size: 10
+        }
+    ]
+};
+
+//Function to create a new player
+function createPlayer(name, team, position) {
+    let player = {
+        name: name,
+        team: team,
+        position: position,
+        stats: {
+            kills: 0,
+            deaths: 0
+        },
+        weapons: [],
+        items: [],
+        effects: []
+    };
+    game.players.push(player);
+    game.teams[team].players.push(player);
+}
+
+//Function to start a new game
+function startGame(mode, map) {
+    if (game.gameModes.includes(mode) && game.maps.includes(map)) {
+        game.round.state = 'in-progress';
+        game.round.startTime = Date.now();
     }
 }
 
-//Function to reload weapon
-function reloadWeapon() {
-    ammo = 10;
-}
-
-//Function to switch weapons
-function switchWeapon(newWeapon) {
-    if (weapons.includes(newWeapon)) {
-        currentWeapon = newWeapon;
-    }
+//Function to end a game
+function endGame() {
+    game.round.state = 'idle';
+    game.round.number++;
 }
